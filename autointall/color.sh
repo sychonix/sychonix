e5cb0041b88d0d89ed32d2578fea8a66c156068f
sychonix <110755590+sychonix@users.noreply.github.com>
2024-11-17 00:48:16 +0700

    Update color.sh

--- autointall/color.sh
+++ autointall/color.sh
@@ -4,7 +4,7 @@ sed -i '/#force_color_prompt=yes/c\force_color_prompt=yes' ~/.bashrc
 # remove old color scheme
 sed -i '/if \[ "\$color_prompt" = yes \]; then/{n;d}' ~/.bashrc
 # add new color scheme
-sed -i '/if \[ "\$color_prompt" = yes \]; then/a PS1="\${debian_chroot:+(\$debian_chroot)}\\\[\\033\[01;32m\\\]\\u\\\[\\033\[01;34m\\\]@\\\[\\033\[01;35m\\\]\\h \\\[\\033[01;36m\\\]\\w \\\[\\033\[01;34m\\\]\\\$ \\\[\\033\[00m\\\]"' ~/.bashrc
+sed -i '/if \[ "\$color_prompt" = yes \]; then/a PS1="\${debian_chroot:+(\$debian_chroot)}\\\[\\033\[01;38;5;82m\\\]\\u\\\[\\033\[01;38;5;220m\\\]@\\\[\\033\[01;38;5;45m\\\]\\h \\\[\\033\[01;38;5;203m\\\]\\w \\\[\\033\[01;38;5;214m\\\]▶ \\\[\\033\[00m\\\]"' ~/.bashrc
 # read .bashrc on login
 echo "source .bashrc" >> ~/.bash_profile
 # read new config in current session
